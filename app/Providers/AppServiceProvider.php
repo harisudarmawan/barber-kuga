290011dc784ef4f392fdf5f67c7abccd3696b916
khoirul-mustofa <khoirulmustofa102@gmail.com>
2026-02-07 09:03:50 +0700

    core: fix not loaded helpers.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Booking;
 use App\Observers\BookingObserver;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (! function_exists('setting')) {
+            Log::warning('Helper function "setting" is not defined. Please check if app/helpers.php is loaded.');
+        }
         Booking::observe(BookingObserver::class);
 
         if (app()->environment('production')) {
--- app/helpers.php
+++ app/helpers.php
@@ -1,18 +1,18 @@
 <?php
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\Cache;
 
-if (!function_exists('setting')) {
+if (! function_exists('setting')) {
     /**
      * Get or set application settings
-     * 
+     *
      * Usage:
      * - Get: setting('FONNTE_TOKEN')
      * - Get with default: setting('FONNTE_TOKEN', 'default-value')
      * - Set: setting('FONNTE_TOKEN', 'new-value')
-     * 
-     * @param string $key
-     * @param mixed $value
+     *
+     * @param  mixed  $value
      * @return mixed
      */
     function setting(string $key, $value = null)
@@ -21,17 +21,15 @@ if (!function_exists('setting')) {
         if (func_num_args() === 2) {
             return Setting::set($key, $value);
         }
-        
+
         // Otherwise, get the setting
         return Setting::get($key, $value);
     }
 }
 
-if (!function_exists('settings')) {
+if (! function_exists('settings')) {
     /**
      * Get all settings as an array
-     * 
-     * @return array
      */
     function settings(): array
     {
